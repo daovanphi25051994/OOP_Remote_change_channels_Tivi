f20bb9150c71032da14aea97c49e745e86705750
DaoVanPhi <daovanphi25051994@gmail.com>
2020-03-05 13:41:07 +0700

    app edited

--- js/app.js
+++ js/app.js
@@ -1,37 +1,36 @@
 const ARRAY_BUTTON = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const VOLUME_DEFAULT = 50;
+const VOLUME_DEFAULT = 0.5;
 let arrayVideos = ["video/vtv1.mp4", "video/vtv2.webm"];
 
 let Remote = function () {
     this.button = ARRAY_BUTTON;
     this.changeVolumeTV = function (tv, volume) {
         tv.changeVolume(volume);
-    }
+    };
     this.changeChannelTV = function (tv, channel) {
         tv.changeChannel(channel);
     }
-}
-
+};
 let Television = function () {
     this.status = false;
     this.volume = VOLUME_DEFAULT;
     this.channel = 1;
     this.changeChannel = function (channel) {
         this.channel = channel;
-    }
+    };
     this.changeVolume = function (volume) {
         this.volume = volume;
     }
-}
+};
 let sony = new Television();
 let remote = new Remote();
 
 function televisionChangeChannel(channel) {
     document.getElementById("video").src = arrayVideos[channel - 1];
+    document.getElementById("video").volume = VOLUME_DEFAULT;
 }
 function changeChannel(channel) {
     if (sony.status) {
-        console.log(channel);
         remote.changeChannelTV(sony, channel);
         televisionChangeChannel(channel);
     }
@@ -43,26 +42,24 @@ function changeStatus() {
     } else {
         sony.status = true;
         document.getElementById("video").src = arrayVideos[sony.channel - 1];
+        document.getElementById("video").volume = VOLUME_DEFAULT;
     }
 }
 function plusVolume() {
     let volume = document.getElementById("video").volume;
-    volume += 0.03;
+    volume += 0.1;
     document.getElementById("video").volume = volume;
-    console.log(volume);
 }
 function minusVolume() {
     let volume = document.getElementById("video").volume;
-    volume -= 0.03;
+    volume -= 0.1;
     document.getElementById("video").volume = volume;
-    console.log(volume);
 }
 function changeStatusVolume() {
     let statusVolume = document.getElementById("video").muted
-    if (statusVolume){
+    if (statusVolume) {
         document.getElementById("video").muted = false;
-    }
-    else {
+    } else {
         document.getElementById("video").muted = true;
     }
 }
